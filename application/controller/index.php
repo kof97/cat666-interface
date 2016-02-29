cf79c9f91ccbd5a589d03ba3cf862a5f5c8b59c5
kof97 <1048434786@qq.com>
2016-02-29 21:37:20 +0800

    test complete

--- application/controller/index.php
+++ application/controller/index.php
@@ -11,65 +11,65 @@ if (!defined('ACC')) exit('this script access allowed');
 
 class Index extends MrController
 {
-	public function init()
-	{
-		var_dump($this->db());
-		echo 0;
+    public function init()
+    {
+        var_dump($this->db());
+        echo 0;
 
-	}
+    }
 
-	/**
-	 * get the recommend videos
-	 * 
-	 * @return json
-	 */
-	public function getRecommend()
-	{
-		$res = $this->model("db")->getRecommend();
+    /**
+     * get the recommend videos
+     * 
+     * @return json
+     */
+    public function getRecommend()
+    {
+        $res = $this->model("db")->getRecommend();
 
-		$res = json_encode($res);
+        $res = json_encode($res);
 
-		echo $res;
+        echo $res;
 
-	}
+    }
 
-	/**
-	 * get videos' information by catgory.
-	 * the uri segment 3 is the category id.
-	 * 
-	 * @return json
-	 */
-	public function getCat()
-	{
-		$id = uriSegment(3);
-		$res = $this->model("db")->getCat($id);
+    /**
+     * get videos' information by catgory.
+     * the uri segment 3 is the category id.
+     * 
+     * @return json
+     */
+    public function getCat()
+    {
+        $id = uriSegment(3);
+        $res = $this->model("db")->getCat($id);
 
-		$res = json_encode($res);
+        $res = json_encode($res);
 
-		echo $res;
+        echo $res;
 
-	}
+    }
 
-	/**
-	 * get search results.
-	 * 
-	 * @return int 0 the request key is empty.
-	 */
-	public function search()
-	{
-		$key = post("key");
+    /**
+     * get search results.
+     * 
+     * @return int 0 the request key is empty.
+     */
+    public function search()
+    {
+        $key = post("key");
 
-		if (is_array($key) || empty($key) || trim($key) == "") {
-			echo 0;
-			return 0;
-		}
+        if (is_array($key) || empty($key) || trim($key) == "") {
+            echo 0;
+            return 0;
+        }
 
-		$res = $this->model("db")->search($key);
-		$res = json_encode($res);
+        $res = $this->model("db")->search($key);
+        $res = json_encode($res);
 
-		echo $res;
+        echo $res;
 
-	}
+    }
 
 }
 
--- application/model/db.php
+++ application/model/db.php
@@ -11,105 +11,105 @@ if (!defined('ACC')) exit('this script access allowed');
 
 class Db extends MrModel
 {
-	// to save the database object
-	public $db;
+    // to save the database object
+    public $db;
 
-	/**
-	 * get the database object.
-	 * 
-	 * @return void
-	 */
-	function __construct()
-	{
-		$this->db = $this->db();
+    /**
+     * get the database object.
+     * 
+     * @return void
+     */
+    function __construct()
+    {
+        $this->db = $this->db();
 
-	}
+    }
 
-	/**
-	 * get the recommend videos.
-	 * 
-	 * @return array
-	 */
-	public function getRecommend()
-	{	
-		$result = array();
-		$arr = array();
+    /**
+     * get the recommend videos.
+     * 
+     * @return array
+     */
+    public function getRecommend()
+    {   
+        $result = array();
+        $arr = array();
 
-		// $cat(name => id)
-		$cat = array();
+        // $cat(name => id)
+        $cat = array();
 
-		// get top category list
-		$c = $this->db->query("SELECT id, name from nav where parentid = 0");
-		while ($p = $c->fetch_assoc()) {
-			$catName = $p['name'];
-			$cat["$catName"] =  $p["id"];
-			
-		}
+        // get top category list
+        $c = $this->db->query("SELECT id, name from nav where parentid = 0");
+        while ($p = $c->fetch_assoc()) {
+            $catName = $p['name'];
+            $cat["$catName"] =  $p["id"];
+            
+        }
 
-		// get 4 videos that order by viewcounts
-		$res = $this->db->query("SELECT * from videoinfo order by viewcounts limit 0, 4");
-		while ($r = $res->fetch_assoc()) {
-			array_push($arr, $r);
-		}
-		$result["!@#$%^&*0!@#$%^&*"] = $arr;
-		$arr = array();
+        // get 4 videos that order by viewcounts
+        $res = $this->db->query("SELECT * from videoinfo order by viewcounts limit 0, 4");
+        while ($r = $res->fetch_assoc()) {
+            array_push($arr, $r);
+        }
+        $result["!@#$%^&*0!@#$%^&*"] = $arr;
+        $arr = array();
 
-		// get every categorys top 4
-		foreach ($cat as $name => $id) {
+        // get every categorys top 4
+        foreach ($cat as $name => $id) {
 
-			$res = $this->db->query("SELECT * from videoinfo where fid = $id order by viewcounts limit 0, 4");
-			while ($r = $res->fetch_assoc()) {
-				array_push($arr, $r);
-			}
-			$result["!@#$%^&*$name!@#$%^&*"] = $arr;
-			$arr = array();
-		}
+            $res = $this->db->query("SELECT * from videoinfo where fid = $id order by viewcounts limit 0, 4");
+            while ($r = $res->fetch_assoc()) {
+                array_push($arr, $r);
+            }
+            $result["!@#$%^&*$name!@#$%^&*"] = $arr;
+            $arr = array();
+        }
 
-		return $result;
+        return $result;
 
-	}
+    }
 
-	/**
-	 * get videos' information by catgory.
-	 * 
-	 * @param int $id category id.
-	 * @return array
-	 */
-	public function getCat($id)
-	{
-		$arr = array();
+    /**
+     * get videos' information by catgory.
+     * 
+     * @param int $id category id.
+     * @return array
+     */
+    public function getCat($id)
+    {
+        $arr = array();
 
-		$res = $this->db->query("SELECT * from videoinfo where fid = $id");
-		while ($r = $res->fetch_assoc()) {
-			array_push($arr, $r);
-		}
+        $res = $this->db->query("SELECT * from videoinfo where fid = $id");
+        while ($r = $res->fetch_assoc()) {
+            array_push($arr, $r);
+        }
 
-		return $arr;
+        return $arr;
 
-	}
+    }
 
-	/**
-	 * get search results.
-	 *
-	 * @param string $key key.
-	 * @return array
-	 */
-	public function search($key)
-	{	
-		$arr = array();
-	  	$keys = "%" . $key . "%";
+    /**
+     * get search results.
+     *
+     * @param string $key key.
+     * @return array
+     */
+    public function search($key)
+    {   
+        $arr = array();
+        $keys = "%" . $key . "%";
 
-	  	$stmt = $this->db->prepare(" SELECT * FROM videoinfo WHERE description like ? ");
-	  	$stmt->bind_param("s", $keys);
-	  	$stmt->execute();
+        $stmt = $this->db->prepare(" SELECT * FROM videoinfo WHERE description like ? ");
+        $stmt->bind_param("s", $keys);
+        $stmt->execute();
 
-	  	$res = $stmt->get_result();
-	  	while ($r = $res->fetch_assoc()) {
-	  		array_push($arr, $r);
-	  	}
-	
-		return $arr;
+        $res = $stmt->get_result();
+        while ($r = $res->fetch_assoc()) {
+            array_push($arr, $r);
+        }
+    
+        return $arr;
 
-	}
-	
+    }
+    
 }

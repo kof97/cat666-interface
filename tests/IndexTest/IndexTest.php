1ade32cc5a3e674fbbd8ae20172f2e5eb3629ea5
kof97 <1048434786@qq.com>
2016-02-29 20:31:17 +0800

    add tests

--- tests/IndexTest/IndexTest.php
+++ tests/IndexTest/IndexTest.php
@@ -2,18 +2,31 @@
 
 define("DS", DIRECTORY_SEPARATOR);
 define("NOW_PATH", dirname(__FILE__) . DS);
+define("BASE_URI", "http://localhost/cat666-interface/");
 
 include_once(NOW_PATH . "../../" . DS . "index.php");
 
 
 class IndexTest extends PHPUnit_Framework_TestCase
 {
+	public $ch;
+
+	function __construct()
+	{
+		$this->ch = curl_init();
+	}
 
 	public function testInit()
 	{
-		$seg = uriSegment(1);
+		curl_setopt($this->ch, CURLOPT_URL, BASE_URI . "index.php/index/init");
+		curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($this->ch, CURLOPT_SSL_VERIFYHOST, false);
+		curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
 
-		$this->assertEmpty($seg);
+		$res = curl_exec($this->ch);
+
+		//var_dump($res);
+		$this->assertEmpty($res);
 
 	}
 	

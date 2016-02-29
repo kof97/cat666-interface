0ad5578741bff2db6baa609d4c73669129a60d17
kof97 <1048434786@qq.com>
2016-02-29 21:34:53 +0800

    test complete

--- tests/IndexTest/IndexTest.php
+++ tests/IndexTest/IndexTest.php
@@ -9,77 +9,77 @@ include_once(NOW_PATH . "../../" . DS . "index.php");
 
 class IndexTest extends PHPUnit_Framework_TestCase
 {
-	public $ch;
+    public $ch;
 
-	function __construct()
-	{
-		$this->ch = curl_init();
+    function __construct()
+    {
+        $this->ch = curl_init();
 
-	}
+    }
 
-	public function testInit()
-	{
-		curl_setopt($this->ch, CURLOPT_URL, BASE_URI . "index.php/index/init");
-		curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, false);
-		curl_setopt($this->ch, CURLOPT_SSL_VERIFYHOST, false);
-		curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
+    public function testInit()
+    {
+        curl_setopt($this->ch, CURLOPT_URL, BASE_URI . "index.php/index/init");
+        curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($this->ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
 
-		$res = curl_exec($this->ch);
+        $res = curl_exec($this->ch);
 
-		$this->assertStringMatchesFormat("object(mysqli)%a[\"client_info\"]%w=>%wstring(%d)%a", $res);
+        $this->assertStringMatchesFormat("object(mysqli)%a[\"client_info\"]%w=>%wstring(%d)%a", $res);
 
-	}
+    }
 
-	public function testGetRecommend()
-	{
-		curl_setopt($this->ch, CURLOPT_URL, BASE_URI . "index.php/index/getRecommend");
-		curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, false);
-		curl_setopt($this->ch, CURLOPT_SSL_VERIFYHOST, false);
-		curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
+    public function testGetRecommend()
+    {
+        curl_setopt($this->ch, CURLOPT_URL, BASE_URI . "index.php/index/getRecommend");
+        curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($this->ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
 
-		$res = curl_exec($this->ch);
+        $res = curl_exec($this->ch);
 
-		$this->assertStringMatchesFormat("%a!@#$%^&*%a!@#$%^&*%a", $res);
+        $this->assertStringMatchesFormat("%a!@#$%^&*%a!@#$%^&*%a", $res);
 
-	}
+    }
 
-	public function testGetCat()
-	{
-		$num = rand(0, 1000);
-		curl_setopt($this->ch, CURLOPT_URL, BASE_URI . "index.php/index/getCat/" . $num);
-		curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, false);
-		curl_setopt($this->ch, CURLOPT_SSL_VERIFYHOST, false);
-		curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
+    public function testGetCat()
+    {
+        $num = rand(0, 1000);
+        curl_setopt($this->ch, CURLOPT_URL, BASE_URI . "index.php/index/getCat/" . $num);
+        curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($this->ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
 
-		$res = curl_exec($this->ch);
+        $res = curl_exec($this->ch);
 
-		$this->assertStringMatchesFormat("[%A]", $res);
+        $this->assertStringMatchesFormat("[%A]", $res);
 
-	}
+    }
 
-	public function testSearch()
-	{
-		$_POST['key'] = "321";
-		curl_setopt($this->ch, CURLOPT_URL, BASE_URI . "index.php/index/search");
-		curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, false);
-		curl_setopt($this->ch, CURLOPT_SSL_VERIFYHOST, false);
-		curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
+    public function testSearch()
+    {
+        $_POST['key'] = "321";
+        curl_setopt($this->ch, CURLOPT_URL, BASE_URI . "index.php/index/search");
+        curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($this->ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
 
-		$res = curl_exec($this->ch);
-		if (trim($res) != "0") {
-			$this->assertStringMatchesFormat("[%A]", $res);
-		}
-		
-		$this->assertEquals(0, $res);
+        $res = curl_exec($this->ch);
+        if (trim($res) != "0") {
+            $this->assertStringMatchesFormat("[%A]", $res);
+        }
+        
+        $this->assertEquals(0, $res);
 
-	}
+    }
 
-	function __destruct()
-	{
-		curl_close($this->ch);
-		
-	}
-	
+    function __destruct()
+    {
+        curl_close($this->ch);
+        
+    }
+    
 }
 
 
